8a2d17dd4c80fc4946d0f51ed4a3036de9a1aa34
Jacob Moore <jacob.moore@GCCYJQ72J1WK.local>
2019-09-17 11:34:02 -0400

    Exercise1 part 1

---
+++ Exercise 1 Console/ConVolCylStep1.py
@@ -0,0 +1,17 @@
+print("Volume of a Cylinder Formula: ")
+#input
+name = input ("What is your name: ") #takes users name
+radius = input ("Input radius(cm): ") #input
+radius = (int)(radius) #cast to int
+
+height = input ("Input height(cm): ") #input
+height = (int)(height) #input
+#what imputs are needed to calculate the volume of a cylinder?
+
+#process
+volume = 3.14*radius*radius*height
+#what formula is used to calculate the volume of a cylinder?
+
+#output
+print (name + ", the volume is: ",volume)
+#what is important about the output?
