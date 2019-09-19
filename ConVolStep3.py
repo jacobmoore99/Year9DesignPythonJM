e84ba4a124f607837f55d007bcc2b80b10faa060
Jacob Moore <jacob.moore@GCCYJQ72J1WK.local>
2019-09-19 15:22:55 -0400

    exercise1part2

--- ConVolStep3.py
+++ ConVolStep3.py
@@ -2,22 +2,32 @@ import math
 print("Volume of a Cylinder Formula: ")
 #input
 name = input ("What is your name: ") #takes users name
-radius = input("\n\tInput radius(cm): ") #input
-radius = (int)(radius) #cast to int
+height = 1
+radius = 1
 
-height = input ("\n\tInput height(cm): ") #input
-height = (int)(height) #input
-#what imputs are needed to calculate the volume of a cylinder?
+while radius != 0 or height != 0:
 
-#process
-volume = math.pi*radius*radius*height
-volume = round(volume, 2)
-#what formula is used to calculate the volume of a cylinder?
+	radius = input("\n\tInput radius(cm): ") #input
+	radius = (int)(radius) #cast to int
 
-#output
-print("Hi "+name+"!")
-print("Give a cylinder with:")
-print("Radius = "+str(radius))
-print("Height = "+str(height))
-print("The volume is: "+str(volume)) 
-#what is important about the output?
+	height = input ("\tInput height(cm): ") #input
+	height = (int)(height) #input
+	#what imputs are needed to calculate the volume of a cylinder?
+
+	#process
+
+	if (radius >= 0 and height >= 0):
+
+		volume = math.pi*radius*radius*height
+		volume = round(volume, 2)
+		#what formula is used to calculate the volume of a cylinder?
+
+		#output
+		print("Hi "+name+"!")
+		print("Give a cylinder with:")
+		print("Radius = "+str(radius))
+		print("Height = "+str(height))
+		print("The volume is: "+str(volume)) 
+		#what is important about the output?
+	else:
+		print ("\n\t\tYou have entered an invalid value.")
