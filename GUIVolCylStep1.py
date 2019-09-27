8a15d3a9a1a044d8dfdf1df0da2cf5198e18f720
Jacob Moore <jacob.moore@GCCYJQ72J1WK.local>
2019-09-27 11:16:44 -0400

    Gui step 1

---
+++ GUIVolCylStep1.py
@@ -0,0 +1,18 @@
+import math
+
+def calcVolumeCylinder(radius, height):
+	
+	if radius >= 0 and height >= 0:
+		volume = math.pi * pow(radius, 2) * height
+		volume = round(volume, 2)
+		return volume
+	else:
+		return -1
+
+
+
+
+print ("Start Program")
+result = calcVolumeCylinder(23, 4)
+print (result)
+print ("END Program")
